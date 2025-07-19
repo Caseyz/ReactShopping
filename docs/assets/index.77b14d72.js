66c9aa93e7108688cfc4ddbde1cd01d92f3e2028
zhengkaixuan <zhengkaixuan96@outlook.com>
2025-07-19 17:01:14 +0800

    feat: 🎸 complete work

--- docs/assets/index.77b14d72.js
+++
@@ -1,2 +0,0 @@
-import{j as r,F as s,a as e,L as a}from"./index.d4e41fd8.js";const o=()=>r(s,{children:[e("div",{children:"401"}),e(a,{to:"/login",style:{color:"blue"},children:"Login"})]});export{o as default};
-//# sourceMappingURL=index.77b14d72.js.map
--- docs/assets/index.b1989e09.js
+++
@@ -1,2 +0,0 @@
-import{a as r}from"./index.d4e41fd8.js";const e=()=>r("div",{children:"404"});export{e as default};
-//# sourceMappingURL=index.b1989e09.js.map
